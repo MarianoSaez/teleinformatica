c7592a63b2d84ed64c8dfb32529cf9d1811b4062
MarianoSaez <mm.saez@alumno.um.edu.ar>
2022-05-04 21:20:38 -0300

    Fixing ruteo #0005

--- routing.py
+++ routing.py
@@ -113,7 +113,7 @@ class Main:
             # Regla de ruteo para ir de Central a Sucursales
             net["r0"].cmd(f"ip route add {SUCIP.format(suc + 1, 0)}/24 via {WANIP.format(8*(suc + 1) - 7)}")
 
-            for i in range(1, SUCRANGE + 1):
+            for i in range(1, SUCNO + 1):
                 # Regla de ruteo para ir desde Sucursal hacia otras sucursales
                 print(f"ip route add {SUCIP.format(i + 1, 0)}/24 via {WANIP.format(8 * (i + 1) - 2)}")
                 net[f"r{suc + 1}"].cmd(f"ip route add {SUCIP.format(i + 1, 0)}/24 via {WANIP.format(8 * (i + 1) - 2)}")
