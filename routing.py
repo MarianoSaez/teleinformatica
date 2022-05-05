d6ca84056d896ca8ee9d68ac8f85a7bfdbae9ebc
MarianoSaez <mm.saez@alumno.um.edu.ar>
2022-05-04 21:00:24 -0300

    Fixing ruteo #0001

--- routing.py
+++ routing.py
@@ -114,6 +114,7 @@ class Main:
 
             for i in SUCRANGE:
                 if i != suc:
+                    print("ENTRO")
                     # Regla de ruteo para ir desde Sucursal hacia otras sucursales
                     net[f"r{suc + 1}"].cmd(f"ip route add {SUCIP.format(i + 1, 0)}/24 via {WANIP.format(8 * (i + 1) - 2)}")
                     # Regla de ruteo para ir desde router de Sucursal hacia routers de sucursales
